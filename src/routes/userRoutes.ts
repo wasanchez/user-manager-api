8d9f2d56b5557ba862142715f4baee8ed9e913e0
wsanchez <wasnchez@icloud.com>
2023-09-23 12:51:26 +0900

    New method to update user info. New schema to validate user data. New dto.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { UserController } from "../controllers/usercontroller";
+import { UserController } from "../controllers/UserController";
+import UpdateUserSchema from "../schemas/updateuserschema";
+import SchemaValidatorMiddleware from "../common/middlewares/schemavalidatormiddleware";
+
 const router = express.Router();
 const controller = new UserController();
 
 router.get("/", controller.getAll );
 router.get("/:id", controller.get);
+router.patch(
+    "/:id",
+    [SchemaValidatorMiddleware.verify(UpdateUserSchema)],
+    controller.updateUserInfo
+);
 
 export = router;
